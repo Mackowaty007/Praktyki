6a28a984da946d4d59fe718231d574219678cedc
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2020-09-17 16:58:48 +0200

    Add files via upload

--- LittleGridProject/test.js
+++ LittleGridProject/test.js
@@ -1,25 +1,28 @@
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
+//rozmiar siatki jest do ustawienia
+var gridSizeX = 5;
+var gridSizeY = 5;
 
 ctx.clearRect(0,0,800,800);
 var x = 0
 var y = 0
-
+//funkcja do rysowania prostokątów czarnych
 function draw(){
-  ctx.fillRect(x,y,100,100);
-  x += 200;
-
+  ctx.fillRect(x,y,gridSizeX,gridSizeY);
+  x += gridSizeX * 2;
 }
+//to wyznacza gdzie mają pojawiać się prostokąty
 while (y<800){
   while (x<800){
     draw()
   }
-  y += 100
-  if (y % 200 == 0){
+  y += gridSizeY
+  if (y % (2*gridSizeY) == 0){
     x=0
   }
   else{
-    x = 100
+    x = gridSizeX
   }
 }
