db917ee309cb485879713b8f1b12c10d81e2544f
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2020-09-28 23:19:08 +0200

    Add files via upload

--- SpaceShootEmUpWith classes/script.js
+++ SpaceShootEmUpWith classes/script.js
@@ -3,12 +3,23 @@ canv = document.getElementById("gameScreen")
 ctx = canv.getContext("2d")
 const playerShip = document.getElementById('source')
 const fireball = document.getElementById('fireball')
+const space = document.getElementById('space')
+var enemyShip = document.getElementById('enemyShip')
 setInterval(gameLoop,1)
 //classes
 class EnemyClass{
   constructor(){
-    this.x = Math.floor(Math.round()*(canv.width-0+1)+0);
+    this.x = Math.floor(Math.random()*((canv.width - enemySize)-0+10)+0);
     this.y = 0;
+  }
+  colision(){
+    if(this.x < fireball1.x + fireballSize && this.x + enemySize > fireball1.x && this.y > fireball1.y - enemySize && this.y < fireball1.y + fireballSize/*|| this.x < fireball2.x || this.x < fireball3.x*/){
+      enemyShip = document.getElementById('explosion')
+      setTimeout(function(){alert("you Lose :()")},500)
+    }
+  }
+  move(){
+    this.y += enemySpeed;
   }
 }
 
@@ -26,18 +37,19 @@ class FireballClass{
 const playerSize = 100;
 const fireballSize = 40;
 const enemySize = 90;
+var enemySpeed = 1
 var x = (canv.width / 2) - (playerSize /2);
 var y = canv.height - playerSize;
 var fireball1;
-var fireball2;
-var fireball3;
+//var fireball2;
+//var fireball3;
 var fireballSpeed = 1;
 var enemy1;
-var enemy2;
+//var enemy2;
 
 fireball1 = new FireballClass;
-fireball2 = new FireballClass;
-fireball3 = new FireballClass;
+//fireball2 = new FireballClass;
+//fireball3 = new FireballClass;
 enemy1 = new EnemyClass;
 window.addEventListener('mousemove',function(e){
   x = e.x - (playerSize/2) - 10;
@@ -46,21 +58,19 @@ window.addEventListener('mousemove',function(e){
 window.addEventListener('click',function(e){
   fireball1.x = x + ((playerSize / 2)-(fireballSize/2));
   fireball1.y = y - 10;
-  fireball1.isFlying = true;
 })
 function gameLoop(){
   draw()
   fireball1.projectile()
+  enemy1.move()
+  enemy1.colision()
 }
 
 function draw(){
-  ctx.fillStyle = "#00a3cc"
-  ctx.fillRect(0,0,canv.width,canv.height)
+  ctx.drawImage(space,0,0,canv.width,canv.height)
   ctx.drawImage(fireball,fireball1.x,fireball1.y,fireballSize,fireballSize)
   //ctx.drawImage(fireball,fireball2.x,fireball2.y,fireballSize,fireballSize)
   //ctx.drawImage(fireball,fireball3.x,fireball3.y,fireballSize,fireballSize)
   ctx.drawImage(playerShip,x,y,playerSize,playerSize)
-  ctx.fillStyle = "#cc0000"
-  ctx.fillRect(enemy1.x,enemy1.y,enemySize,enemySize)
-  console.log(fireball1.x)
+  ctx.drawImage(enemyShip,enemy1.x,enemy1.y,enemySize,enemySize)
 }
