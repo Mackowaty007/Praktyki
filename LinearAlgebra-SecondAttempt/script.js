12f9836868ebb0034754ebce50b539136596d18e
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2021-03-20 21:22:57 +0100

    Add files via upload

--- LinearAlgebra-SecondAttempt/script.js
+++ LinearAlgebra-SecondAttempt/script.js
@@ -8,10 +8,14 @@ ix = 10
 iy = 0
 verteceX = 0
 verteceY = 0
+offset = 450
+squareSize = 
+
+ctx.lineWidth = 2
 
 window.addEventListener("mousemove",function(e){
-    x = e.x / 100
-    y = e.y / 100
+    x = (e.x - offset)
+    y = (e.y - offset) 
 })
 document.addEventListener("keydown",function(e){
     switch(e.keyCode){
@@ -33,15 +37,25 @@ function main(){
     ctx.fillStyle = "white"
     ctx.fillRect(0,0,canv.width,canv.height)
 
+    ctx.strokeStyle = "blue"
+    ctx.beginPath()
+    ctx.moveTo(offset,offset)
+    ctx.lineTo(x + offset,y + offset)
+    ctx.stroke()
+    ctx.strokeStyle = "red"
+    ctx.beginPath()
+    ctx.moveTo(offset,offset)
+    ctx.lineTo(ix + offset,iy + offset)
+    ctx.stroke()
+
+
     ctx.fillStyle = "green"
-    for(i=0;i<10;i++){
-        for(u=0;u<10;u++){
-            verteceX = x*i + ix * u
-            verteceY = y*i + iy * u
+    for(i=-10;i<10;i++){
+        for(u=-10;u<10;u++){
+            verteceX = (x*i + ix * u) + offset
+            verteceY = (y*i + iy * u) + offset
 
             ctx.fillRect (verteceX,verteceY,10,10)
         }
     }
-    
-
 }
